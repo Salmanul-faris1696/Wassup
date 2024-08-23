6ff8b9ff8ee81b5ca81a595a148f77b06d062e58
Salmanul-faris1696 <salmansb0786@gamil.com>
2024-08-23 11:22:11 +0530

    ui design completed | 2:30:00

---
+++ Frontend/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,31 @@
+import MessageInput from "./MessageInput"
+import Messages from "./Messages"
+import NoChatSelected from "./NoChatSelected"
+
+const MessageContainer = () => {
+	const noChatSelected = true;
+  return (
+	<div className="md:min-w-[450px] flex flex-col">
+		{
+			noChatSelected ? (
+				<NoChatSelected/>
+			):(
+				<>
+		{/* headerSection */}
+		<div className="bg-slate-500 px-4 py-2 mb-2 ">
+			<span className="label-text">To:</span>
+			<span className="text-gray-900 font-bold "> Shanffu</span>
+		</div>
+
+		<Messages/>
+		<MessageInput/>
+		
+		</>
+			)
+		}
+		
+	</div>
+  )
+}
+
+export default MessageContainer
